2aa1fcad5ac74642bd7ef05f91ee9572213d2911
Equim <791457156@qq.com>
2016-12-27 19:09:33 +0800

    Fixed body animation; removed comments used for version control.

--- src/main_page.js
+++ src/main_page.js
@@ -2,12 +2,11 @@
 $(function() {
     // 初始化，只会执行一次
     $.LessFlag = false;
-    //$.OverIframe = false;
     $("#blurStack").css("opacity", "1");     //在moz下无效，原因不明
     $("body").animate({backgroundSize: "104%"}, 1200);
 
     // 按钮与iframe的鼠标悬停响应
-    $("button,input,iframe").on({
+    $("button, input, iframe").on({
         mouseover: function() {
             $("#monokuroStack").stop().animate({opacity: 0}, 2000);
             if (!$.LessFlag) {
@@ -26,31 +25,21 @@ $(function() {
 
     // 单独设置iframe的鼠标悬停，考虑到跨域iframe的mousemove尚未解决
     $("iframe").mouseover(function() {
-        //$.OverIframe = true;
         $("#monokuroStack").stop().animate({opacity: 1}, 2000);
     });
 
     // 响应鼠标移动，以改变透视视图
     var persp = function(e) {
-        // if (!$.OverIframe) {
-            $("body").css("backgroundPosition", 
-                - e.pageX * 0.03 + "px " + 
-                - e.pageY * 0.03 + "px");
-        // } else {
-        //     $("body").stop().animate({
-        //         backgroundPositionX: - e.pageX * 0.03 + "px",
-        //         backgroundPositionY: - e.pageY * 0.03 + "px"
-        //     }, 500, "swing", function() {
-        //         $.OverIframe = false;
-        //     });
-        // }
+        $("body").css("backgroundPosition", 
+            - e.pageX * 0.03 + "px " + 
+            - e.pageY * 0.03 + "px");
     };
     $(window).mousemove(persp);
 
     // 跨域iframe事件，未解决！！！！
-     // document.getElementById('cloud').addEventListener("mousemove", function(e) {
-     //     alert(e.pageX);
-     // }, false);
+    // document.getElementById('cloud').addEventListener("mousemove", function(e) {
+    //     alert(e.pageX);
+    // }, false);
     // 这个方法确实能响应跨域iframe的"mousemove"，但是拿不到eventArgs也是白搭
     // var monitor = setInterval(function(){
     //     var elem = document.activeElement;
@@ -61,14 +50,11 @@ $(function() {
 
     // More按钮的单击响应
     $("#moreBtn").click(function() {
-        //var breaks = document.getElementById("breaks");
         var breaks = $("#breaks");
         if (!$.LessFlag) {
             for (var i = 1; i <= 50; i++) {
-                //breaks.innerHTML = "<br/>" + breaks.innerHTML;
                 breaks.append("<br/>");
             };
-            //document.getElementById("moreBtn").innerHTML = "Less";
             $(this).text("Less");
 
             // 这个方法有点粗暴，但确实有效
@@ -77,9 +63,8 @@ $(function() {
                     "Actually I made this scroll bar so that you can see the full background image :p" +
                 "</p>" + 
                 "<p>Just feel her breath~</p>");
-            $("#blurStack,#monokuroStack").height($("body").height());
-            //location.hash = "#breaks";
-            $("body").animate({scrollTop: $("#breaks").offset().top}, 600);    //平滑跳转锚点
+            $("#blurStack, #monokuroStack").height($("body").height());
+            $("html, body").animate({scrollTop: $("#breaks").offset().top}, 600);    //平滑跳转锚点
 
             // 呼吸式循环动画，颇有函数式编程的感觉
             $("body").stop();
@@ -95,20 +80,19 @@ $(function() {
 
             $.LessFlag = true;
         } else {
-            $("#moreBtn,#hmmBtn").animate({opacity: 0}, 250);
-            // ! ISSUE !
-            // 响应速度变慢了，可能会卡死！随之而来的还有更多的问题！
-            $("body").animate({scrollTop: "0"}, 1000, function() {
+            $("#moreBtn, #hmmBtn").animate({opacity: 0}, 250);
+            $("body").stop(true, true);       // debug一整天的结果，就是加上了这句话，然后就解决了(
+            $("html, body").animate({scrollTop: "0px"}, 1000, function() {
                 breaks.text("");
                 $("#moreBtn").text("More");
-                $("#blurStack,#monokuroStack").height("100%");
-                $("#moreBtn,#hmmBtn").animate({opacity:1}, 500);
+                $("#blurStack, #monokuroStack").height("100%");
+                $("#moreBtn, #hmmBtn").animate({opacity:1}, 500);
                 $.LessFlag = false;
             });
         }
     });
 
-    // Hmm按钮的单击响应，已改为用Bootstrap Modal
+    // Hmm按钮的单击响应，现已改为用Bootstrap Modal
     //$("#hmmBtn").click(function()  {            // QAQ，真的要习惯大括号不换行吗……
     //    if (confirm("I said it is under construction! >.<\nHowever you may enjoy these awesome songs first.\n\nWould you like to view it on GitHub?")) {
             //window.navigate("https://github.com/Equim-chan/equim-chan.github.io");        // 本地测试无效
